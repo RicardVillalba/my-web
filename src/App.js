89daabab184f88334106ff14b9a55b94a172889f
Ricard Villalba <ricardsax@gmail.com>
2020-06-11 20:40:54 +0200

    snake and apple created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Snake from "./Snake";
+import Apple from "./Apple";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const getRandomPosition = () => {
+  let min = 1;
+  let max = 98;
+  let x = Math.floor((Math.random() * (max - min + 1) + min) / 2) * 2;
+  let y = Math.floor((Math.random() * (max - min + 1) + min) / 2) * 2;
+  return [x, y];
+};
+
+class App extends Component {
+  state = {
+    apple: getRandomPosition(),
+    snakeDots: [
+      [0, 0],
+      [2, 0],
+    ],
+  };
+
+  render() {
+    return (
+      <div className="gameArea">
+        <Snake snakeDots={this.state.snakeDots} />
+        <Apple dot={this.state.apple} />
+      </div>
+    );
+  }
 }
-
 export default App;
