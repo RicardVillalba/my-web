8295175cdbd1b291bf3b148b8a0d883e81a854d0
Ricard Villalba <ricardsax@gmail.com>
2020-06-15 10:46:16 +0200

    link and navlink implemented

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import "./../App.css";
+import {Link ,NavLink} from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav className="nav-wrapper red darken-3">
       <div className="container">
         <ul className="right">
-          <li><a href="/">Home</a></li>
-          <li><a href='/snakeGame'>snakeGame</a></li>
+          <li><Link to="/">Home</Link></li>
+          <li><NavLink to='/snakeGame'>snakeGame</NavLink></li>
         </ul>
       </div>
     </nav> 
